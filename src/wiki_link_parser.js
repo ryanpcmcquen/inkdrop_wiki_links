9f296f6962723f05c530927b2c53256997644454
Ryan McQuen <rpcm@linux.com>
2020-12-20 16:19:22 -0800

    This is the remark bug.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- src/wiki_link_parser.js
+++ src/wiki_link_parser.js
@@ -3,24 +3,24 @@ function locator(value, fromIndex) {
 }
 
 // https://regex101.com/r/Tsi5wM/1
+// Works:
 const RE_WIKI = /\[([^\]]+)\]{2}/gm;
+// https://regex101.com/r/Tsi5wM/2
+// Breaks:
+// const RE_WIKI = /\[([^\[\]]+)\]{2}/gm;
 
 function tokenizer(eat, value, silent) {
     const match = RE_WIKI.exec(value);
     if (match) {
-        if (silent || match.length < 1 || !match[1]) {
+        if (silent || match.length < 1) {
             return true;
         }
-        // debugger;
-        // if (!match[1]) {
-        // return true;
-        // }
+        debugger;
 
         const result = eat(match[0])({
             type: "wiki_link",
             value: match[1].trim(),
         });
-        // debugger;
         return result;
     }
 }
