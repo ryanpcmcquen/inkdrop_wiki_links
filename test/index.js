c1c339949e334c4fc407b658b9c8f1a24d265dd2
Ryan McQuen <rpcm@linux.com>
2020-12-23 14:51:51 -0800

    Do not use reserved word in mobile test.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
-const package = "wiki_links";
+const thisPackage = "wiki_links";
 
-test.serial.cb(package, (t) => {
+test.serial.cb(thisPackage, (t) => {
     global.window.inkdrop.packages.setPackageMainModule = (name, p) => {
-        t.is(name, package);
+        t.is(name, thisPackage);
         t.is(p instanceof Object, true);
         t.is(typeof p.activate, "function");
         t.is(typeof p.deactivate, "function");
         t.end();
     };
 
-    require(`../lib/${package}`);
+    require(`../lib/${thisPackage}`);
 });
