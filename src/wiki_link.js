e8e87bffebba75053ec3c25f079171cabf88c0a9
Ryan McQuen <rpcm@linux.com>
2020-12-24 16:56:54 -0800

    More clean up!
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- src/wiki_link.js
+++ src/wiki_link.js
@@ -1,86 +1,78 @@
 "use babel";
 import * as React from "react";
-import PropTypes from "prop-types";
 
-const db = inkdrop && inkdrop?.main?.dataStore?.getLocalDB();
+if (!inkdrop.isMobile) {
+    const db = inkdrop && inkdrop?.main?.dataStore?.getLocalDB();
+}
 
 const createRemarkWikiLink = (OriginalSpan) => {
-    return class WikiLink extends React.Component {
-        static propTypes = {
-            children: PropTypes.arrayOf(PropTypes.string),
-        };
+    const WikiLink = (props) => {
+        const link = props.children[0];
 
-        render() {
-            const link = this.props.children[0];
+        if (link) {
+            try {
+                const attributes = {
+                    // href: `inkdrop://note/${link}`,
+                    onClick: (event) => {
+                        event.preventDefault();
+                        event.stopPropagation();
 
-            if (link) {
-                try {
-                    const attributes = {
-                        // href: `inkdrop://note/${link}`,
-                        onClick: (event) => {
-                            if (db) {
-                                event.preventDefault();
-                                event.stopPropagation();
-                                db.utils
-                                    .search(`title:${link}`)
-                                    .then((note) => {
-                                        let noteToOpenId;
-                                        if (
-                                            note?.docs &&
-                                            note.docs.length > 0
-                                        ) {
-                                            noteToOpenId = note.docs[0]._id;
-                                        } else {
-                                            const {
-                                                editingNote,
-                                            } = inkdrop.store.getState();
-                                            noteToOpenId = db.notes.createId();
-                                            db.notes.put({
-                                                ...editingNote,
-                                                _id: noteToOpenId,
-                                                _rev: undefined,
-                                                body: "",
-                                                title: link,
-                                                createdAt: Date.now(),
-                                                updatedAt: Date.now(),
-                                            });
-                                        }
-                                        inkdrop.commands.dispatch(
-                                            document.body,
-                                            "core:open-note",
-                                            { noteId: noteToOpenId }
-                                        );
+                        if (inkdrop.isMobile) {
+                        } else {
+                            db.utils.search(`title:${link}`).then((note) => {
+                                let noteToOpenId;
+                                if (note?.docs && note.docs.length > 0) {
+                                    noteToOpenId = note.docs[0]._id;
+                                } else {
+                                    const {
+                                        editingNote,
+                                    } = inkdrop.store.getState();
+                                    noteToOpenId = db.notes.createId();
+                                    db.notes.put({
+                                        ...editingNote,
+                                        _id: noteToOpenId,
+                                        _rev: undefined,
+                                        body: "",
+                                        title: link,
+                                        createdAt: Date.now(),
+                                        updatedAt: Date.now(),
                                     });
+                                }
+                                inkdrop.commands.dispatch(
+                                    document.body,
+                                    "core:open-note",
+                                    { noteId: noteToOpenId }
+                                );
+                            });
+                        }
 
-                                return false;
-                            }
-                        },
-                        renderError: (error) => {
-                            return (
-                                <span className="ui error message mde-error-message">
-                                    {error.message}
-                                </span>
-                            );
-                        },
-                        ...this.props,
-                    };
-                    if (OriginalSpan) {
+                        return false;
+                    },
+                    renderError: (error) => {
                         return (
-                            <OriginalSpan {...this.props}>
-                                {this.props.children}
-                            </OriginalSpan>
+                            <span className="ui error message mde-error-message">
+                                {error.message}
+                            </span>
                         );
-                    } else {
-                        return <span {...attributes}>{link}</span>;
-                    }
-                } catch (e) {
-                    return <span>{e.message}</span>;
+                    },
+                    ...props,
+                };
+                if (OriginalSpan) {
+                    return (
+                        <OriginalSpan {...props}>{props.children}</OriginalSpan>
+                    );
+                } else {
+                    return <span {...attributes}>{link}</span>;
                 }
-            } else {
-                return <span>Invalid Wiki Link</span>;
+            } catch (e) {
+                return <span>{e.message}</span>;
             }
+        } else {
+            return <span>Invalid Wiki Link</span>;
         }
     };
+
+    return WikiLink;
 };
 
 export default createRemarkWikiLink;
